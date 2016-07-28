a3450b5e868974c5a847550e1ea3947d8ad0cc69
Francis Lawlor <franlawlor0@gmail.com>
2016-07-28 22:50:16 +0100

    Made e-mail parameter optional.

--- pagechecker.sh
+++ pagechecker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#First parameter should be twitter username. Second parameter should be e-mail address to send prompt to.
+#First parameter should be twitter username. Second optional parameter should be e-mail address to send prompt to.
 
-# Check if user is failing to enter the two required parameters.
-if [ ! $# -eq 2 ]
-	then echo You must enter two arguments, the username you want to monitor and the e-mail to send notifications to!
-	exit
+# Check if user is failing to enter the required parameters.
+if [ ! $# -eq 1 ] && [ ! $# -eq 2 ]
+then 
+	echo "You must enter the username whose account you want to monitor and/or an e-mail address to send notifications!"
+	exit 
 fi
 
-# Check if the indicated username is valid.
-if curl --output /dev/null --head --fail --silent "https://twitter.com/$1"
+# Check if the indicated username is valid by checking if the corresponding webpage exists.
+if curl --output /dev/null --head --fail --silent "https://twitter.com/$1"	
 then
 	echo "Running..."
 else
@@ -20,20 +21,23 @@ fi
 # Parse number of tweets user currently has for comparison when user tweets/removes tweets.
 prev=`curl -s "https://twitter.com/$1" | grep -o -P '(?<=title=").*(?= Tweets)'`
 
-# Remove any commas from the parse number.
+# Remove any commas from the parsed number.
 prev="${prev//,}"
 
 while [ 1 ];
 do
 	current=`curl -s "https://twitter.com/$1" | grep -o -P '(?<=title=").*(?= Tweets)'`
 	
-	# Remove any commas from the parse number.
+	# Remove any commas from the parsed number.
 
 	current="${current//,}"
 
 	if [ $current -gt $prev ]
 	then
-       		`echo "A new tweet!" | mail -s "New tweet from @$1!" $2`
+		if [ -n "$2" ]
+		then
+       			`echo "Check @$1's twitter for their latest tweet!" | mail -s "New tweet from @$1!" $2`
+		fi
 		
 		# Add image to your notification by adding the path as a parameter to the the notify-send command.
 		# Mine is called "smirk.png and is stored in the Pictures directory.
